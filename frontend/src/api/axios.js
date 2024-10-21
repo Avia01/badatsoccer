dbc43feb891aa23c9f9f7daaa4ef01f5a432e59d
Dean Uziel <dean2910997@gmail.com>
2024-10-21 20:31:03 +0300

    change api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://python-flask-webapp-t.azurewebsites.net',
+    // baseURL: 'https://python-flask-webapp-t.azurewebsites.net',
+    baseURL: 'https://goaltime.onrender.com',
     // baseURL: 'http://localhost:5000',
 });
