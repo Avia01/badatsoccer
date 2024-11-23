e85b103d837959917eaafccde3d6e8c9c248cdea
Avia01 <aviariel01@gmail.com>
2024-11-24 01:04:39 +0200

    update baseUrl

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: 'https://goaltime.onrender.com',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://goaltime.onrender.com',
+    // baseURL: 'http://localhost:5000',
 });
